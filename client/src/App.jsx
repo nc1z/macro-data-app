9bb0291372a938920d3b0716e265dcde8dee6d6e
nc1z <nc1zdev@gmail.com>
2022-11-22 14:31:34 +0800

    basic routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Account from "./routes/Account";
+import Home from "./routes/Home";
 import Login from "./routes/Login";
+import Signup from "./routes/Signup";
 
 function App() {
-  return <div>Test</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/account" element={<Account />} />
+    </Routes>
+  );
 }
 
 export default App;
