3a74f3544893d63af46b40c6cc15457d0af4a7a0
nc1z <nc1zdev@gmail.com>
2022-11-23 17:24:35 +0800

    update account page

--- client/src/routes/Account.jsx
+++ client/src/routes/Account.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { UserAuth } from "../context/AuthContext";
 
 const Account = () => {
-  return <div>Account</div>;
+  const { user } = UserAuth();
+  const dateTime = new Date(user.metadata?.lastLoginAt * 1);
+
+  if (!user) {
+    return <div>Not logged in</div>;
+  }
+
+  return (
+    <section>
+      <div>Email: {user.email}</div>
+      <div>Last Logged In: {dateTime.toString()}</div>
+      <div>Watchlist:</div>
+    </section>
+  );
 };
 
 export default Account;
