80134ef8b627781eb4a307c57766e8cfffcf6bb5
nc1z <nc1zdev@gmail.com>
2022-11-25 12:44:22 +0800

    Install chartJs & init

--- client/src/components/TickerChart.jsx
+++ client/src/components/TickerChart.jsx
@@ -1,10 +1,57 @@
 import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const TickerChart = ({ ticker }) => {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  };
+
+  const data = {
+    labels: ticker.data.dates,
+    datasets: [
+      {
+        label: "Dataset",
+        data: ticker.data.values,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
   return (
     <div>
       <div>{ticker.description}</div>
       <div>{ticker.ticker}</div>
+      <Line options={options} data={data} />
     </div>
   );
 };
