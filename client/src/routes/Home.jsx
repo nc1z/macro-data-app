6d18cdefc3382f6b71715645abd45e97ddc4c283
nc1z <nc1zdev@gmail.com>
2022-11-25 11:01:48 +0800

    update data fetching & state

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useRef, useState } from "react";
 import Dashboard from "../components/Dashboard";
+import axios from "axios";
 
 const Home = () => {
+  const [data, setData] = useState({});
+  const handleFetch = useRef(() => {});
+
+  handleFetch.current = async () => {
+    try {
+      const { data: response } = await axios.get(
+        "https://www.econdb.com/api/series/?page=1&format=json"
+      );
+      console.log(response.results);
+      setData(response.results);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    handleFetch.current();
+  }, [handleFetch]);
+
   return (
     <section>
-      <Dashboard />
+      <Dashboard data={data} />
     </section>
   );
 };
