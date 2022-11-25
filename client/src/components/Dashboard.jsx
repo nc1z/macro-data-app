b213c04fe32b362bd10aee6b512f11111e0a7b13
nc1z <nc1zdev@gmail.com>
2022-11-25 12:47:59 +0800

    Added Chart Container

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 import TickerChart from "./TickerChart";
+import styled from "styled-components";
+
+const ChartContainer = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 const Dashboard = ({ data }) => {
   if (!data) {
@@ -13,11 +19,11 @@ const Dashboard = ({ data }) => {
   }
 
   return (
-    <>
+    <ChartContainer>
       {data.map((ticker) => (
         <TickerChart ticker={ticker} key={ticker.ticker} />
       ))}
-    </>
+    </ChartContainer>
   );
 };
 
