7a940407cc26874bddcd5b02252b135238b40db9
nc1z <nc1zdev@gmail.com>
2022-11-28 17:11:25 +0800

    debuggig tests

--- client/src/__tests__/components/Login.test.jsx
+++ client/src/__tests__/components/Login.test.jsx
@@ -1,3 +1,38 @@
+import { beforeEach, describe, it, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Login from "../../routes/Login";
 
-describe(Login, () => {});
+describe(Login, () => {
+  const onSubmit = vi.fn();
+
+  beforeEach(() => {
+    onSubmit.mockClear();
+    render(<Login onSubmit={onSubmit} />);
+  });
+
+  it("onSubmit is called when all fields pass validation", async () => {
+    userEvent.type(getEmail(), "test@test.com");
+    userEvent.type(getPassword(), "testing");
+    userEvent.click(getLoginButton());
+
+    await waitFor(() => {
+      expect(onSubmit).toHaveBeenCalledTimes(1);
+    });
+
+    expect(onSubmit).toHaveBeenCalledWith({ lazy: true });
+  });
+});
+
+// Functions ------------------->
+
+const getEmail = () =>
+  screen.getByRole("textbox", {
+    name: /email address/i,
+  });
+const getPassword = () => container.querySelector("#password");
+
+const getLoginButton = () =>
+  screen.getByRole("button", {
+    name: /sign in/i,
+  });
