1b34fbeddcff95b61f954d4d386e34fea6111e23
nc1z <nc1zdev@gmail.com>
2022-11-30 10:29:28 +0800

    resolved login test case error - unnecessary multiple renders beforeEach

--- client/src/__tests__/components/Login.test.jsx
+++ client/src/__tests__/components/Login.test.jsx
@@ -5,6 +5,7 @@ import Login from "../../routes/Login";
 import { AuthContextProvider } from "../../context/AuthContext";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import authMock from "../mockAuth";
+import { act } from "react-dom/test-utils";
 
 describe("Login", () => {
   // Mock States & Functions
@@ -35,37 +36,25 @@ describe("Login", () => {
     handleLogin.mockClear();
     setEmail.mockClear();
     setPassword.mockClear();
-    render(
-      <BrowserRouter>
-        <AuthContextProvider>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Login
-                  setEmail={setEmail}
-                  setPassword={setPassword}
-                  handleSubmit={handleLogin}
-                />
-              }
-            />
-          </Routes>
-        </AuthContextProvider>
-      </BrowserRouter>
-    );
   });
-
-  it("handleLogin is called on Sign In Button Clicked", async () => {
-    userEvent.click(getLoginButton());
-
-    await waitFor(() => {
-      expect(
-        firebaseAuth().signInAndRetrieveDataWithEmailAndPassword
-      ).toHaveBeenCalledWith(emailState, passwordState);
-      expect(handleLogin).toHaveBeenCalledTimes(1);
-    });
-    expect(handleLogin).toHaveReturned();
-  });
+  render(
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Login
+                setEmail={setEmail}
+                setPassword={setPassword}
+                handleSubmit={handleLogin}
+              />
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 
   it("setState triggers when input field onChange", async () => {
     expect(getEmail()).toHaveProperty("id", "email");
@@ -73,6 +62,7 @@ describe("Login", () => {
 
     await userEvent.type(getEmail(), "test@test.com");
     await userEvent.type(getPassword(), "testing");
+
     await waitFor(() => {
       expect(setEmail).toHaveBeenCalledWith("test@test.com");
       expect(setPassword).toHaveBeenCalledWith("testing");
@@ -83,6 +73,18 @@ describe("Login", () => {
     // Mock State Updated
     expect(setEmail).toHaveReturnedWith("test@test.com");
     expect(setPassword).toHaveReturnedWith("testing");
+  });
+
+  it("handleLogin is called on Sign In Button Clicked", async () => {
+    userEvent.click(getLoginButton());
+
+    await waitFor(() => {
+      expect(
+        firebaseAuth().signInAndRetrieveDataWithEmailAndPassword
+      ).toHaveBeenCalledWith("test@test.com", "testing");
+      expect(handleLogin).toHaveBeenCalledTimes(1);
+    });
+    expect(handleLogin).toHaveReturned();
   });
 });
 
